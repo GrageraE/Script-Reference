d62afab081c242d0b87c4d12a7b3fcec8d3c3413
CosmoDX <benjaminsalazarballadares@gmail.com>
2019-11-28 14:22:02 -0300

    Added the main website and added a "Simple Template".

---
+++ JS/Rbuttons.js
@@ -0,0 +1,18 @@
+//Redirect Buttons
+function redirectToMain()
+{
+    var redirectLink = "https://cosmoxdd.github.io/Script-Reference/";
+    window.location.href = redirectLink;
+}
+
+function redirectToGithub()
+{
+    var redirectLink = "https://github.com/CosmoXDD/Script-Reference";
+    window.location.href = redirectLink;
+}
+
+function helpProject()
+{
+    var redirectLink = "https://github.com/CosmoXDD/Script-Reference/help.html";
+    window.location.href = redirectLink;
+}
